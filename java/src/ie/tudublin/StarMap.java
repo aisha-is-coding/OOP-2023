0baf978111dace5a0e6617cd7d083575b87218c2
aisha-is-coding <aishaadebimpe@gmail.com>
2023-02-21 14:20:34 +0000

    pls work

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -6,7 +6,7 @@ public class StarMap extends PApplet
 {
 	public void settings()
 	{
-		size(500, 500);
+		size(800, 800);
 	}
 
 	public void setup() {
@@ -17,9 +17,29 @@ public class StarMap extends PApplet
 		
 	}
 
+	public void drawGrid()
+	{
+		stroke(255);
+		float border = 50.f;
+
+		int count = 10;
+		float gap = (width - (border * 2.0f)) / (float) count;
+
+		for(int i = -5; i <= 5; i++)
+		{
+			float x = border + (gap * (i + 5));
+			line(x, border,x, height - border);
+			line(border, x, width - border, x);
+		}
+	}
 		
 	public void draw()
 	{	
+		//line(120, 80, 340, 80);
+		//stroke(120);
+		drawGrid();
 		strokeWeight(2);		
 	}
 }
+
+
